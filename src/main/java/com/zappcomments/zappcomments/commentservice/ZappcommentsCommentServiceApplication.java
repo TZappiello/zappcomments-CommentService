d6185783950a49cc8e9fffaf8033984e56806269
tzappiello <tzappiello@gmail.com>
2025-06-03 17:14:23 -0300

    Novos arquivos do projeto

---
+++ src/main/java/com/zappcomments/zappcomments/commentservice/ZappcommentsCommentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zappcomments.zappcomments.commentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZappcommentsCommentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZappcommentsCommentServiceApplication.class, args);
+	}
+
+}
